20857508838bebe3dad10f01654f1c04de2e2c09
LeHai1004 <lehai191006@gmail.com>
2026-04-07 14:21:56 +0700

    Bước 4: Hoàn thành DbContext

---
+++ BT_CodeFirst_QuanLyQuanCafe/CafeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BT_CodeFirst_QuanLyQuanCafe.Models;
+
+namespace BT_CodeFirst_QuanLyQuanCafe
+{
+    public class CafeDbContext : DbContext
+    {
+        public DbSet<LoaiMon> LoaiMons { get; set; }
+        public DbSet<MonAn> MonAns { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-JJUNDI09;Database=QuanLyQuanCafeDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
